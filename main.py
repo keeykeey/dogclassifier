a899eb370f4dec98846aaf8c22050b4f4229e076
Shin Fujito <shinfujito@ShinnoMacBook-Pro.local>
2019-12-15 21:14:41 +0900

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-main program
+#main program
 
+def chooseDB():
+    pass
+
+def sample():
+    pass
+
+
+
+
+
+def create_model():
+    pass
+
+def predict():
+    pass
+
+
+if __name__ == "__main__":
+    predict()
 
 
 
