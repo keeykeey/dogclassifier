f57ed444e06c23c28789debcf951f356e617eccd
keeykeey <sh1n_fuj1to@icloud.com>
2019-12-28 18:11:37 +0900

    original library

--- tools.py
+++ tools.py
@@ -1,4 +1,8 @@
 
+import numpy as np
+import pandas as pd
+
+
 def math(a,b):
     print(a + b)
 
@@ -9,15 +13,24 @@ class sample():
     def hold_out():
         print('aa')
                 
+    
+
+
+
+
+
+
+
+
+
+
 
 one = [1,2,3,4]
 two = [2,4,6,8]
 three = [3,6,9,12]
 
-a = sample(one=one,two=two,three=three)
-print(a.col)
-print(type(a.col))
-
+a = pd.DataFrame(sample(one=one,two=two,three=three).col)
+print(a)
 
 
 
