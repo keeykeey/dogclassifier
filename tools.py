d5f36d8dfdd1fa54606540d0a32e6f6189870a20
keeykeey <sh1n_fuj1to@icloud.com>
2019-12-28 18:03:45 +0900

    make odiginal library

--- tools.py
+++ tools.py
@@ -1,14 +1,22 @@
 
+def math(a,b):
+    print(a + b)
+
 class sample():
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,**col):
+        self.col = col 
 
     def hold_out():
         print('aa')
                 
 
+one = [1,2,3,4]
+two = [2,4,6,8]
+three = [3,6,9,12]
 
-
+a = sample(one=one,two=two,three=three)
+print(a.col)
+print(type(a.col))
 
 
 
